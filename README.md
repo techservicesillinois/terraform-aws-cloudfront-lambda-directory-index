55fa4c6d1bd13e18b31a2e527abf2ee4f40c26c0
Jon R. Roma <JonRoma@users.noreply.github.com>
2019-11-07 14:27:01 -0600

    Migrate to Terraform 0.12 (#3)

---
+++ README.md
@@ -0,0 +1,3 @@
+# cloudfront-lambda-directory-index
+
+[![Build Status](https://drone.techservices.illinois.edu/api/badges/techservicesillinois/terraform-aws-cloudfront-lambda-directory-index/status.svg)](https://drone.techservices.illinois.edu/techservicesillinois/terraform-aws-cloudfront-lambda-directory-index)
--- main.tf
+++ main.tf
@@ -9,21 +9,17 @@ data "aws_iam_role" "selected" {
 data "aws_region" "current" {}
 
 locals {
-  account_id = "${data.aws_caller_identity.current.account_id}"
-  region     = "${data.aws_region.current.name}"
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
 }
 
 resource "aws_lambda_function" "default" {
   description   = "Serve directory index.html files for Cloudfront"
-  function_name = "${var.function_name}"
-  runtime       = "${var.runtime}"
+  function_name = var.function_name
+  runtime       = var.runtime
   handler       = "index.handler"
-  publish       = "true"
-  role          = "${data.aws_iam_role.selected.arn}"
+  publish       = true
+  role          = data.aws_iam_role.selected.arn
   s3_bucket     = "drone-${local.region}-${local.account_id}"
   s3_key        = "cloudfront-directory-index/cloudfront-directory-index.zip"
-}
-
-output "qualified_arn" {
-  value = "${aws_lambda_function.default.qualified_arn}"
 }
